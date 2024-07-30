3b6532efe5169373bd1d08ef572cd9bd99677978
Gerodote <lg3dx6fd@gmail.com>
2024-07-30 21:47:32 +0000

    updated to use version 2.0 of AddBoost.cmake
    fix styling

--- cmake/packages/addboost.cmake
+++ cmake/packages/addboost.cmake
@@ -1,7 +1,5 @@
 include(${CMAKE_CURRENT_LIST_DIR}/../getCPM.cmake)
 
 CPMAddPackage(
-  NAME AddBoost.CMake
-  GIT_TAG main
-  GITHUB_REPOSITORY Arniiiii/AddBoost.cmake
+  NAME AddBoost.CMake URL https://github.com/Arniiiii/AddBoost.cmake/archive/refs/tags/2.0.tar.gz
 )
