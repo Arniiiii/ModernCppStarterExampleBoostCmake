1663f0a70e4dd8d6d2c060dec8a720815118f84e
Gerodote <lg3dx6fd@gmail.com>
2024-07-31 16:57:44 +0000

    update example to use addboost.cmake ver. 2.1

--- cmake/packages/addboost.cmake
+++ cmake/packages/addboost.cmake
@@ -1,5 +1,4 @@
 include(${CMAKE_CURRENT_LIST_DIR}/../getCPM.cmake)
 
-CPMAddPackage(
-  NAME AddBoost.CMake URL https://github.com/Arniiiii/AddBoost.cmake/archive/refs/tags/2.0.tar.gz
-)
+CPMAddPackage( NAME AddBoost.CMake VERSION 2.1 URL https://github.com/Arniiiii/AddBoost.cmake/archive/refs/tags/2.1.tar.gz)
+
