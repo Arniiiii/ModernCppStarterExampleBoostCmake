d6074c26399bf950976226f969f663b306449bde
Gerodote <lg3dx6fd@gmail.com>
2024-08-08 18:47:20 +0000

    update example to use new version of AddBoost.cmake
    fix styling
    
    myeah,  python is not working sometimes...

--- cmake/packages/addboost.cmake
+++ cmake/packages/addboost.cmake
@@ -1,4 +1,7 @@
 include(${CMAKE_CURRENT_LIST_DIR}/../getCPM.cmake)
 
-CPMAddPackage( NAME AddBoost.CMake VERSION 2.1 URL https://github.com/Arniiiii/AddBoost.cmake/archive/refs/tags/2.1.tar.gz)
-
+CPMAddPackage(
+  NAME AddBoost.CMake
+  VERSION 3.0
+  URL https://github.com/Arniiiii/AddBoost.cmake/archive/refs/tags/3.0.tar.gz
+)
