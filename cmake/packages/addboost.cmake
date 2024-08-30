93718c68eeba1e2a6faf938d99bb4904335679a2
Arniiiii <lg3dx6fd@gmail.com>
2024-08-30 20:50:10 +0000

    update to use version of AddBoost.cmake 3.3

--- cmake/packages/addboost.cmake
+++ cmake/packages/addboost.cmake
@@ -2,6 +2,6 @@ include(${CMAKE_CURRENT_LIST_DIR}/../getCPM.cmake)
 
 CPMAddPackage(
   NAME AddBoost.CMake
-  VERSION 3.0
-  URL https://github.com/Arniiiii/AddBoost.cmake/archive/refs/tags/3.0.tar.gz
+  VERSION 3.3
+  URL https://github.com/Arniiiii/AddBoost.cmake/archive/refs/tags/3.3.tar.gz
 )
